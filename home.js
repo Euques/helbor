a65ca6260c2c7891aae3f96b42b9c82db6ac6a2f
Euques Wabes <60795401+Euques@users.noreply.github.com>
2024-11-30 21:13:05 -0300

    Update home.js

--- home.js
+++ home.js
@@ -1,6 +1,4 @@
-script type="module"
-
-      import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js';
+  import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js';
       import {
         getFirestore,
         collection,
