222c8115f457fe431275a7db4c57aab94134ac32
Euques Wabes <60795401+Euques@users.noreply.github.com>
2024-11-30 22:30:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 // Script para controlar o menu sanduíche
 document.getElementById('menu-toggle').addEventListener('click', function() {
     var menu = document.getElementById('menu');
-    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+    // Toggle para abrir e fechar o menu
+    if (menu.style.transform === 'translateX(0px)') {
+        menu.style.transform = 'translateX(-250px)';
+    } else {
+        menu.style.transform = 'translateX(0px)';
+    }
 });
 
 // Função para carregar o conteúdo no iframe
